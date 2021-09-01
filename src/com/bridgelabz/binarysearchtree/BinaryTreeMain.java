d658f8aaa1cb379181b3b774cb4ac6a71221b049
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-01 22:05:00 +0530

    ADDED: initialized project structure

---
+++ src/com/bridgelabz/binarysearchtree/BinaryTreeMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.binarysearchtree;
+
+public class BinaryTreeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/bridgelabz/binarysearchtree/MyBinaryNode.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.binarysearchtree;
+
+public class MyBinaryNode {
+
+}
