d4f7e239819cb8b98eae29696d0a434d8de8b46f
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-01 23:13:58 +0530

    ADDED: added 13 nodes to myBinaryTree

--- src/com/bridgelabz/binarysearchtree/BinaryTreeMain.java
+++ src/com/bridgelabz/binarysearchtree/BinaryTreeMain.java
@@ -7,6 +7,16 @@ public class BinaryTreeMain {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
 		int size=myBinaryTree.getSize();
 		System.out.println(size);
 	}
