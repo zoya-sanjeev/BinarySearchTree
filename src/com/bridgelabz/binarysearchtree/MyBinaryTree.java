db027a9203dae534eab03a8724d481125222b8f2
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-01 22:41:39 +0530

    ADDED: add and addRecursively method added to MyBinaryTree

--- src/com/bridgelabz/binarysearchtree/MyBinaryTree.java
+++ src/com/bridgelabz/binarysearchtree/MyBinaryTree.java
@@ -1,5 +1,24 @@
 package com.bridgelabz.binarysearchtree;
 
-public class MyBinaryTree {
+public class MyBinaryTree <K extends Comparable<K>>{
+	private MyBinaryNode<K> root;
+	
+	public void add(K key) {
+		this.root=this.addRecursively(root,key);
+	}
+	
+	public MyBinaryNode<K> addRecursively(MyBinaryNode<K> current,K key){
+		if(current==null)
+			return new MyBinaryNode<>(key);
+		int compareResult=key.compareTo(current.key);
+		if(compareResult==0) return current;
+		if(compareResult<0) {
+			current.left=addRecursively(current.left, key);
+		}
+		else {
+			current.right=addRecursively(current.right, key);
+		}
+		return current;
+	}
 
 }
