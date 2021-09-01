af5397894f0beee2c394e25c377c484b0a1e1938
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-01 22:17:15 +0530

    ADDED: MyBinaryNode created

--- src/com/bridgelabz/binarysearchtree/MyBinaryNode.java
+++ src/com/bridgelabz/binarysearchtree/MyBinaryNode.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.binarysearchtree;
 
-public class MyBinaryNode {
-
+public class MyBinaryNode<K extends Comparable<K>> {
+	
+	K key;
+	MyBinaryNode<K> left;
+	MyBinaryNode<K> right;
+	
+	public MyBinaryNode(K key) {
+		this.key=key;
+		this.left=null;
+		this.right=null;
+	}
 }
